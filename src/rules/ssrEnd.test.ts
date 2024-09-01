f84624a6e037dd516d3b6b106119ec4aff089f76
hacknlove <antonio@hacknlove.org>
2024-09-01 12:53:20 +0200

    test for client side data

---
+++ src/rules/ssrEnd.test.ts
@@ -0,0 +1,23 @@
+import { HTMLRewriter } from "@/test/HTMLRewriter";
+import { RewriterContext } from "types";
+import { describe, it, expect } from "vitest";
+import { ssrEnd } from "./ssrEnd";
+
+describe("ssrEnd", () => {
+  it("move some end elements to the end of the body", async () => {
+    const rewriter = new HTMLRewriter();
+    const rewriterContext: RewriterContext = {
+      clientSideData: {
+        foo: "bar",
+      },
+    };
+
+    ssrEnd(rewriter, rewriterContext);
+
+    const result = await rewriter.transform(`<body></body>`);
+
+    expect(result).toBe(
+      `<body></body><script>window.data={"foo":"bar"};document.dispatchEvent(new Event('on-data-loaded'))</script>`,
+    );
+  });
+});
