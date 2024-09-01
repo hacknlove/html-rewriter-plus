69f335f240cd0b47415ab643c253d7add854f4b9
hacknlove <antonio@hacknlove.org>
2024-09-01 12:27:03 +0200

    tests for head

---
+++ src/rules/head.test.ts
@@ -0,0 +1,31 @@
+import { HTMLRewriter } from "@/test/HTMLRewriter";
+import { RewriterContext } from "types";
+import { describe, it, expect } from "vitest";
+import { ssrHead } from "./head";
+
+describe("ssrHead", () => {
+  it("move some head elements to the end of the head", async () => {
+    const rewriter = new HTMLRewriter();
+    const rewriterContext: RewriterContext = {};
+
+    ssrHead(rewriter, rewriterContext);
+
+    rewriter.on("meta[key='move-this']", {
+      element(element) {
+        // it's responsability of any rule using this feature to add the element to the headElements array. Here we will just hardcode the string, for the sake of this test.
+        rewriterContext.headElements.push(
+          '<meta key="move-this" content="foo"/>',
+        );
+        element.remove();
+      },
+    });
+
+    const result = await rewriter.transform(
+      '<head><title>Page Title</title><meta key="move-this" content="foo"/><link rel="stylesheet" href="styles.css"></head>',
+    );
+
+    expect(result).toBe(
+      '<head><title>Page Title</title><link rel="stylesheet" href="styles.css"><meta key="move-this" content="foo"/></head>',
+    );
+  });
+});
